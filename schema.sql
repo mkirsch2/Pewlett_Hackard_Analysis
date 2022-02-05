70c882ae5c2f653b6945b793c9ca0bffdac028f5
Your Name <you@example.com>
2022-02-05 15:51:30 -0700

    Updated schema

--- schema.sql
+++ schema.sql
@@ -49,6 +49,37 @@ CREATE TABLE titles (
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  PRIMARY KEY (emp_no)
+);
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL
+);
+
+SELECT * FROM departments;
